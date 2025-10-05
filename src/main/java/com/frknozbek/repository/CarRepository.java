3370a09cdb76921925f1a5912b0fa1bfcdc17424
Furkan OZBEK <yer6dans@gmail.com>
2025-10-06 00:07:23 +0300

    feat: update CarRepository methods to return List and Page types; enhance CarRepositoryTest with additional assertions for production year and currency type

--- src/main/java/com/frknozbek/repository/CarRepository.java
+++ src/main/java/com/frknozbek/repository/CarRepository.java
@@ -1,16 +1,20 @@
 package com.frknozbek.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.frknozbek.model.Car;
 import java.util.List;
-import java.util.Optional;
-
+import com.frknozbek.enums.CurrencyType;
 
 @Repository
 public interface CarRepository extends MongoRepository<Car, String> {
 
-    Optional<List<Car>> findAllByProductionYear(Integer productionYear);
+    List<Car> findAllByProductionYear(Integer productionYear);
+    Page<Car> findAllByProductionYear(Integer productionYear, Pageable pageable);
+
+    List<Car> findAllByProductionYearAndCurrencyType(Integer productionYear, CurrencyType currencyType);
 
 }
--- src/test/java/com/frknozbek/repository/CarRepositoryTest.java
+++ src/test/java/com/frknozbek/repository/CarRepositoryTest.java
@@ -3,12 +3,14 @@ package com.frknozbek.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import com.frknozbek.enums.CurrencyType;
 import com.frknozbek.model.Car;
 
 @DataMongoTest
@@ -18,12 +20,39 @@ public class CarRepositoryTest {
     CarRepository carRepository;
 
     @Test
-    void testFindByProductionYearIsLikeIgnoreCase() {
+    void testFindAllByProductionYear() {
 
-        Optional<List<Car>> byProductionYearCars = carRepository.findAllByProductionYear(2017);
+        List<Car> byProductionYearCars = carRepository.findAllByProductionYear(2017);
 
-        byProductionYearCars.get().forEach(car -> {
+        byProductionYearCars.forEach(car -> {
             assertEquals(2017, car.getProductionYear());
+        });
+
+    }
+
+    @Test
+    void testFindAllByProductionYearPage() {
+
+        Page<Car> byProductionYearCars = carRepository.findAllByProductionYear(2017, PageRequest.of(1, 2));
+
+        assertEquals(2,byProductionYearCars.getPageable().getPageSize());
+        assertEquals(3, byProductionYearCars.getTotalElements());
+
+
+        byProductionYearCars.forEach(car -> {
+            assertEquals(2017, car.getProductionYear());
+        });
+
+    }
+
+    @Test
+    void testFindAllByProductionYearAndCurrencyType() {
+
+        List<Car> byProductionYearAndCurrencyTypeCars = carRepository.findAllByProductionYearAndCurrencyType(2017, CurrencyType.USD);
+
+        byProductionYearAndCurrencyTypeCars.forEach(car -> {
+            assertEquals(2017, car.getProductionYear());
+            assertEquals("USD", car.getCurrencyType().name());
         });
 
     }
